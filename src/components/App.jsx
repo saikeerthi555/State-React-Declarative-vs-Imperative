c02406f99f2c65d7cc24d208668d7d90a4023af3
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-01-27 13:42:48 +0530

    imperative programming And this is what we've been doing all along using Javascript. When we say document.getElementById and then we tap into its properties and then we set it equal to something,this is us imperative early telling this element to do something different.And you can achieve exactly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+//declarative programming
+// function App() {
+//   var isDone = true;
+//   const strikeThrough = { textDecoration: "line-through" };
+//   return <p style={isDone ? strikeThrough : null}>Buy Milk</p>;
+// }
+
+//imperative programming
+function strike() {
+  document.getElementById("root").style.textDecoration = "line-through";
+}
+function unstrike() {
+  document.getElementById("root").style.textDecoration = null;
+}
 function App() {
-  var isDone = true;
-  const strikeThrough = { textDecoration: "line-through" };
-  return <p style={isDone ? strikeThrough : null}>Buy Milk</p>;
+  return (
+    <div>
+      <p>Buy Milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unstrike}>Change to strike through</button>
+    </div>
+  );
 }
 
 export default App;
